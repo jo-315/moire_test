81d44e4a870f616aca0f77ab5a1803d8e37dfac0
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-11-17 01:09:08 +0900

    mobile sidebar

--- wp-content/themes/moire/sidebar.php
+++ wp-content/themes/moire/sidebar.php
@@ -5,9 +5,6 @@
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary">
-	<span class="sidebar-mobile-title">
-		<?php esc_html__( 'サイドバー' ) . ''; ?>
-	</span>
 	<div class="sidebar-inner-wrap">
 		<?php
 		dynamic_sidebar('moire-sidebar-1'); ?>
