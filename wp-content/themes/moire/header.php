9e899da46e71a2142faeb5f41b540dd48b613127
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-11-16 13:20:53 +0900

    header toggle

--- wp-content/themes/moire/header.php
+++ wp-content/themes/moire/header.php
@@ -23,37 +23,34 @@
 </head>
 
 <body>
-  <?php //TODO toggle ?>
 	<header role="banner">
 		<div class="header-inner-top">
-			<div class="header-main-navigation-wrap">
-  			<nav class="header-main-navigation">
-					<ul class='header-main-navigation-items-wrap'>
-						<li class='header-main-navigation-item nav_default_underline'>
-							<a href="/">HOME</a>
-						</li>
-						<li class='header-main-navigation-item'>
-							<a href="/about/">ABOUT</a>
-						</li>
-						<li class='header-main-navigation-item'>
-							<a href="/category/blog/">BLOG</a>
-						</li>
-              <li class='header-main-navigation-item'>
-							<a href="/category/event/">イベント一覧</a>
-						</li>
+			<nav class="header-main-navigation">
+				<ul class='header-main-navigation-items-wrap'>
+					<li class='header-main-navigation-item nav_default_underline'>
+						<a href="/">HOME</a>
+					</li>
+					<li class='header-main-navigation-item'>
+						<a href="/about/">ABOUT</a>
+					</li>
+					<li class='header-main-navigation-item'>
+						<a href="/category/blog/">BLOG</a>
+					</li>
             <li class='header-main-navigation-item'>
-							<a href="/category/organization/">団体一覧</a>
-						</li>
-            <li class='header-main-navigation-item'>
-							<a href="/corporation/">団体様へ</a>
-						</li>
-            <li class='header-main-navigation-item'>
-							<a href="/about/recruit/">リクルート</a>
-						</li>
-            <span id='header-nav-underline'></span>
-					</ul>
-  			</nav>
-      </div>
+						<a href="/category/event/">イベント一覧</a>
+					</li>
+          <li class='header-main-navigation-item'>
+						<a href="/category/organization/">団体一覧</a>
+					</li>
+          <li class='header-main-navigation-item'>
+						<a href="/corporation/">団体様へ</a>
+					</li>
+          <li class='header-main-navigation-item'>
+						<a href="/about/recruit/">リクルート</a>
+					</li>
+          <span id='header-nav-underline'></span>
+				</ul>
+			</nav>
 
 			<div class="header-menu-toggle-button-wrap">
 				<button class="header-menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i></button>
--- wp-content/themes/moire/js/functions.js
+++ wp-content/themes/moire/js/functions.js
@@ -20,7 +20,13 @@ jQuery(function($) {
 
 // toggle button
 jQuery(function($) {
-  $('.header-menu-toggle').click(function(){
-      $header.toggleClass('open');
-  });
+  $('.header-menu-toggle').click(() => {
+    const toggleMenue = $('.header-main-navigation')
+    if(toggleMenue.hasClass('active')){
+      toggleMenue.removeClass('active');
+      $('.header-main-navigation').slideUp();
+    } else {
+      toggleMenue.addClass('active');
+      $('.header-main-navigation').slideDown();
+    }  });
 });
