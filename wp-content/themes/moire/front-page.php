5ec467103f64f838967240ccc4bfdeac0ada2642
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-11-21 09:43:05 +0900

    右カラムのアニメーション

--- wp-content/themes/moire/front-page.php
+++ wp-content/themes/moire/front-page.php
@@ -2,15 +2,21 @@
 <?php get_header('front'); //headerの読み込み ?>
 
 <div class='front-page-wrapper'>
-  <div class='main-content'>
-    <div class="moire-title-wrap">
-      <span class='moire-title'>
-        moire
-      </span>
+  <div class="content-effect">
+    <div class='main-content'>
+      <div class="moire-title-wrap">
+        <span class='moire-title'>
+          moire
+        </span>
 
-      <span class="moire-sub-title">
-        阪大生と社会活動を繋げる広報団体
-      </span>
+        <span class="moire-sub-title">
+          阪大生と社会活動を繋げる広報団体
+        </span>
+      </div>
+    </div>
+
+    <div class="right-top-column">
+      私たちmoireは
     </div>
   </div>
 </div>
--- wp-content/themes/moire/js/front.js
+++ wp-content/themes/moire/js/front.js
@@ -1,5 +1,5 @@
 jQuery(function($) {
-  add_circle = function() {
+  add_first_circle = function() {
     const options = {
     	widthRatio: 0.5,
     	heightRatio: 0.5,
@@ -16,10 +16,49 @@ jQuery(function($) {
   }
 
   return new Promise((resolve, reject) => {
-    resolve(add_circle())
+    resolve(add_first_circle())
   }).then(() => {
     return $(".moire-title").fadeIn(7000)
   }).then(() => {
-    return $(".moire-sub-title").fadeIn(9000)
+    return $(".moire-sub-title").fadeIn(8000)
   })
 });
+
+jQuery(function($) {
+  add_random_circle = function() {
+    const options = {
+      widthRatio: Math.random(),
+      heightRatio: Math.random(),
+      delay: 0,
+      gap: 0,
+      effect: "drops-css",
+      effectOptions: {
+        radius: 200,
+        width: Math.random() * 10,
+        duration: 1e3 + Math.random() * 1e3,
+        color: '#f6f6f6',
+        opacity: 0.3
+      }
+    }
+    $(".content-effect").twinkle(options);
+  }
+
+  let topAnimated = false
+
+  $(window).scroll(() => {
+    if (!topAnimated) {
+      topAnimated = true
+
+      setInterval(add_random_circle, 1000);
+
+      $('.main-content').css({
+        'width': '50%',
+        'float': 'left'
+      })
+      $('.moire-sub-title').css('font-size', '25pt')
+      $('.right-top-column')
+        .css('display', 'block')
+        .animate({'width':'50%'})
+    }
+  })
+})
