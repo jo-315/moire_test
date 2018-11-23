eeeb934ed7e7ccb158578f2a4b31903a21197440
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-11-24 00:41:12 +0900

    file 構成

---
+++ wp-content/themes/moire/header-contact-us.php
@@ -0,0 +1,26 @@
+<?php
+// ヘッダー
+?><!DOCTYPE html>
+<?php
+  $category = get_the_category();
+  $cat_name = $category[0]->cat_name;
+?>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php wp_head(); ?>
+</head>
+
+<body class="moire-body">
+  <header role="banner">
+    <?php get_template_part( 'template-parts/header', 'navigation' ); ?>
+    <div class="header-img-wrapper">
+      <div class="content-effect-wrapper">
+        <div id="content-effect">
+          <div class="post-header-title">
+          <span>お問い合わせ</span>
+        </div>
+      </div>
+    </div>
+	</header>
---
+++ wp-content/themes/moire/page-recruit.php
@@ -0,0 +1,34 @@
+<?php
+/*
+カテゴリー毎、投稿一覧
+blog,event,orgで同じもの
+*/
+?>
+
+<?php
+get_header('recruit');
+?>
+
+  <div id="content" class="site-content post-content">
+    <div class="archive-content-wrap">
+			<div id="primary">
+				<main id="main" role="main">
+					<?php
+					/* 投稿記事をloopで表示 */
+					while ( have_posts() ) :
+						the_post();
+
+	          // 投稿記事のアイテム
+						get_template_part( 'template-parts/content' );
+
+					endwhile;
+
+					the_posts_navigation(); // 2ページ目へ
+				?>
+				</main>
+			</div>
+		</div>
+  </div>
+
+<?php
+get_footer();
