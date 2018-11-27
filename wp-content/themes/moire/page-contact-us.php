7526d48a1cd681677a03baab6c84f20e4d98b39a
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-11-28 00:08:48 +0900

    contact

--- wp-content/themes/moire/page-contact-us.php
+++ wp-content/themes/moire/page-contact-us.php
@@ -9,26 +9,10 @@ blog,event,orgで同じもの
 get_header('contact-us');
 ?>
 
-  <div id="content" class="site-content post-content">
-    <div class="archive-content-wrap">
-			<div id="primary">
-				<main id="main" role="main">
-					<?php
-					/* 投稿記事をloopで表示 */
-					while ( have_posts() ) :
-						the_post();
-
-	          // 投稿記事のアイテム
-						get_template_part( 'template-parts/content' );
-
-					endwhile;
-
-					the_posts_navigation(); // 2ページ目へ
-				?>
-				</main>
-			</div>
-		</div>
-  </div>
+<div class="about_content_block">
+  <?php // echo do_shortcode( '[contact-form-7 id="54" title="コンタクトフォーム_default"]' ); ?>
+  <?php echo do_shortcode( '[contact-form-7 id="763" title="Contact form 1"]' ); ?>
+</div>
 
 <?php
 get_footer();
