cee1a5e5f5c2ac77a2b207e691bd6abf90f06f3e
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-11-28 01:33:42 +0900

    member

--- wp-content/themes/moire/page-member.php
+++ wp-content/themes/moire/page-member.php
@@ -9,26 +9,30 @@ blog,event,orgで同じもの
 get_header('member');
 ?>
 
-  <div id="content" class="site-content post-content">
-    <div class="archive-content-wrap">
-			<div id="primary">
-				<main id="main" role="main">
-					<?php
-					/* 投稿記事をloopで表示 */
-					while ( have_posts() ) :
-						the_post();
+<div class="about_content_block">
+  <div class="member_content_wrap">
+    <div class="member_content">
+      <div class="profile_text_title">
+        代表   見城 俊介
+      </div>
 
-	          // 投稿記事のアイテム
-						get_template_part( 'template-parts/content' );
+      <div class="member_img">
+        <img
+          src="<?php echo get_stylesheet_directory_uri(); ?>/images/profile_kenjo.png"
+          class="profile_img"
+        />
+      </div>
 
-					endwhile;
-
-					the_posts_navigation(); // 2ページ目へ
-				?>
-				</main>
-			</div>
-		</div>
+      <div class="profile_text">
+        <div class="profile_text_content">
+          大学に入ってすぐ勧誘されNICE、ひいてはボランティアに関わるように。
+          二回生の冬からはBaseconnect Incでエンジニアインターンに参加。
+          現在はmoireやNICEの活動を行いつつインターンしてます。
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 
 <?php
 get_footer();
