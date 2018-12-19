1813daf10aeb899c980607633e7cbdc35bb67002
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-12-19 11:50:24 +0900

    membar page

--- wp-content/themes/moire/page-member.php
+++ wp-content/themes/moire/page-member.php
@@ -13,12 +13,12 @@ get_header('member');
   <div class="member_content_wrap">
     <div class="member_content">
       <div class="profile_text_title">
-        <span>代表   見城 俊介</span>
+        <span>代表　　見城 俊介</span>
       </div>
 
       <div class="member_img">
         <img
-          src="<?php echo get_stylesheet_directory_uri(); ?>/images/profile_kenjo.png"
+          src="http://moire.xsrv.jp/wp-content/uploads/2018/12/profile_kenjo.png"
           class="profile_img"
         />
       </div>
@@ -28,6 +28,86 @@ get_header('member');
           大学に入ってすぐNICE、ひいてはボランティアに関わるように。
           二回生の冬からはBaseconnect Incでエンジニアインターンに参加。
           現在はmoireやNICEの活動を行いつつインターンしてます。
+        </div>
+      </div>
+    </div>
+
+    <div class="member_content">
+      <div class="profile_text_title">
+        <span>運営メンバー　　野垣 秀天</span>
+      </div>
+
+      <div class="member_img">
+        <img
+          src="http://moire.xsrv.jp/wp-content/uploads/2018/12/profile_hide.png"
+          class="profile_img"
+        />
+      </div>
+
+      <div class="profile_text">
+        <div class="profile_text_content">
+          学生団体ASANTE PROJECT 大阪支部代表 / ALL ABOUT AFRICA 編集担当</br>
+          「自分の目でリアルなアフリカを見て、多くの人にリアルな情報を届ける」がモットー。
+          アフリカ大好きな大学2回生です！
+        </div>
+      </div>
+    </div>
+
+    <div class="member_content">
+      <div class="profile_text_title">
+        <span>ライター　　ナターシャ</span>
+      </div>
+
+      <div class="member_img">
+        <img
+          src="http://moire.xsrv.jp/wp-content/uploads/2018/12/profile_sakie.png"
+          class="profile_img"
+        />
+      </div>
+
+      <div class="profile_text">
+        <div class="profile_text_content">
+          中の人は日本人です。現在ロシアのサンクト=ペテルブルクに留学中。普通の大学3年生。太陽の光を浴びたいが最近の口癖。よろしくお願いいたします。</br>
+        </br>
+        </div>
+      </div>
+    </div>
+
+    <div class="member_content">
+      <div class="profile_text_title">
+        <span>ライター　　小香</span>
+      </div>
+
+      <div class="member_img">
+        <img
+          src="http://moire.xsrv.jp/wp-content/uploads/2018/12/profile_miyata.png"
+          class="profile_img"
+        />
+      </div>
+
+      <div class="profile_text">
+        <div class="profile_text_content">
+          犬を愛する旅行好きな中国語学習者。ただ今中国にて修行中です。老後は犬とともに暮らしたい。
+        </div>
+      </div>
+    </div>
+
+    <div class="member_content">
+      <div class="profile_text_title">
+        <span>ライター　　タロ</span>
+      </div>
+
+      <div class="member_img">
+        <img
+          src="http://moire.xsrv.jp/wp-content/uploads/2018/12/profile_taro.png"
+          class="profile_img"
+        />
+      </div>
+
+      <div class="profile_text">
+        <div class="profile_text_content">
+          ドイツに留学中</br>
+          本場ドイツでビールを飲むも普通に「苦っ」と言ってしまう。ただ、Radler という飲みやすいビールに出会い救われる。
         </div>
       </div>
     </div>
