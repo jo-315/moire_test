f81b0ef7cbed33606f07ece4f6ddbf807b9238e3
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-12-21 09:52:38 +0900

    途中コミット

--- wp-content/themes/moire/front-page.php
+++ wp-content/themes/moire/front-page.php
@@ -47,74 +47,156 @@
 
     <div class="block-wrapper">
       <div class="HP-content-wrapper">
-        <div class="HP-content-column">
-          <div class="HP-content-column-header">
-            <span>
-              No Money Action
-            </span>
-          </div>
-
-          <div class="HP-content-column-logo">
-            <img
-              data-src="<?php echo get_stylesheet_directory_uri(); ?>/images/blog_logo.svg"
-              class="HP_logo lazyload"
-            >
-          </div>
-
-          <div class="HP-content-column-content">
-            No Money Action についての記事を掲載し、発信していきます。
-          </div>
-
-          <div class="HP-content-column-button">
-            <a href='/category/article/'>
-              No Money Action へ
-            </a>
-          </div>
+        <div class="popular_article_wrap_title">
+          <h2>人気記事</h2>
+          <p>
+            各記事の最後に設置されたいいねボタンをクリックすることで結果が変動します。</br>
+            気に入った記事があったらぜひいいねしてください！
+          </p>
         </div>
 
-        <div class="HP-content-column">
-          <div class="HP-content-column-header">
-            <span>団体情報一覧</span>
-          </div>
+        <div class="popular_article">
+          <?php
+            $the_query = new WP_Query(array(
+            	'post_status' => 'published',
+            	'post_type' => 'post',
+            	'orderby' => 'meta_value_num',
+            	'meta_key' => '_liked',
+            	'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1,
+              'posts_per_page'=> '3'
+            ));
 
-          <div class="HP-content-column-logo">
-            <img
-              data-src="<?php echo get_stylesheet_directory_uri(); ?>/images/team_logo.svg"
-              class="HP_logo lazyload"
-            >
-          </div>
+            $count = 1;
 
-          <div class="HP-content-column-content">
-            社会貢献活動を行う団体の情報を手軽に見れるようにし、団体の活動参加を容易にします。
-          </div>
+            while($the_query->have_posts()):$the_query->the_post();
+          ?>
 
-          <div class="HP-content-column-button">
-            <a href='/category/organization/'>
-              団体一覧へ
-            </a>
-          </div>
+          <li class="HP-content-column popular_article-column popular_article-column-<?php echo $count?>">
+            <div class="popular_article_title popular_article_title-<?php echo $count?>">
+              <?php echo $count?>
+            </div>
+            <div class="popular_article_img">
+              <?php the_post_thumbnail('large'); ?>
+            </div>
+
+            <div class="popular_article_content">
+              <div class="popular_article-desc">
+                <div class="popular_article-tag">
+                  <?php
+                    $tags = get_the_tags();
+                    if ($tags) {
+                      foreach($tags as $tag) {
+                        echo '<span>' . $tag->name . '</span>';
+                      }
+                    }
+                  ?>
+                </div>
+                <div class="popular_article-wrap">
+                  <?php
+                    moire_theme_posted_by_no_name()
+                  ?>
+                </div>
+              </div>
+
+              <?php
+                the_title( '<h2 class="archive-item-title">', '</h2>' );
+              ?>
+            </div>
+
+
+            <a href="<?php the_permalink(); ?>"></a>
+          </li>
+
+        <?php
+          $count += 1;
+          endwhile;
+        ?>
         </div>
 
-        <div class="HP-content-column">
-          <div class="HP-content-column-header">
-            <span>イベント情報一覧</span>
+      </div>
+    </div>
+
+    <div class="between_content">
+      <div class="between_content_effect">
+      </div>
+    </div>
+
+    <div class="block-wrapper">
+      <div class="HP-content-wrapper">
+        <div class="HP_content_title">
+          <h2>コンテンツ</h2>
+        </div>
+
+        <div>
+          <div class="HP-content-column">
+            <div class="HP-content-column-header">
+              <span>
+                No Money Action
+              </span>
+            </div>
+
+            <div class="HP-content-column-logo">
+              <img
+                data-src="<?php echo get_stylesheet_directory_uri(); ?>/images/blog_logo.svg"
+                class="HP_logo lazyload"
+              >
+            </div>
+
+            <div class="HP-content-column-content">
+              No Money Action についての記事を掲載し、発信していきます。
+            </div>
+
+            <div class="HP-content-column-button">
+              <a href='/category/article/'>
+                No Money Action へ
+              </a>
+            </div>
           </div>
 
-          <div class="HP-content-column-logo">
-            <img
-              data-src="<?php echo get_stylesheet_directory_uri(); ?>/images/event_logo.svg"
-              class="HP_logo lazyload"
-            >
+          <div class="HP-content-column">
+            <div class="HP-content-column-header">
+              <span>団体情報一覧</span>
+            </div>
+
+            <div class="HP-content-column-logo">
+              <img
+                data-src="<?php echo get_stylesheet_directory_uri(); ?>/images/team_logo.svg"
+                class="HP_logo lazyload"
+              >
+            </div>
+
+            <div class="HP-content-column-content">
+              社会貢献活動を行う団体の情報を手軽に見れるようにし、団体の活動参加を容易にします。
+            </div>
+
+            <div class="HP-content-column-button">
+              <a href='/category/organization/'>
+                団体一覧へ
+              </a>
+            </div>
           </div>
 
-          <div class="HP-content-column-content">
-            社会貢献活動のイベントも発信し、より社会貢献活動に参加しやすい環境を作ります。
-          </div>
+          <div class="HP-content-column">
+            <div class="HP-content-column-header">
+              <span>イベント情報一覧</span>
+            </div>
 
-          <div class="HP-content-column-button">
-            <a href='/category/event'>
-              イベント情報一覧へ
-            </a>
+            <div class="HP-content-column-logo">
+              <img
+                data-src="<?php echo get_stylesheet_directory_uri(); ?>/images/event_logo.svg"
+                class="HP_logo lazyload"
+              >
+            </div>
+
+            <div class="HP-content-column-content">
+              社会貢献活動のイベントも発信し、より社会貢献活動に参加しやすい環境を作ります。
+            </div>
+
+            <div class="HP-content-column-button">
+              <a href='/category/event'>
+                イベント情報一覧へ
+              </a>
+            </div>
           </div>
 
         </div>
