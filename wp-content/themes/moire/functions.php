20849b8cf2e1538019cef99141f40a579d27438d
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-12-31 10:06:02 +0900

    途中コミット

--- wp-content/themes/moire/functions.php
+++ wp-content/themes/moire/functions.php
@@ -101,8 +101,8 @@ if (!(is_admin() )) {
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
-remove_action('wp_head', '‘rsd_link’');
-remove_action('wp_head', '‘wp_shortlink_wp_head’');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_shortlink_wp_head');
 remove_action('wp_head', 'feed_links', 2);
 remove_action('wp_head', 'feed_links_extra', 3);
 
